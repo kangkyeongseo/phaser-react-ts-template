fa4b2e32669648b3d82a41ba97b313e27e1e5bbe
kangkyeongseo <kks_big@naver.com>
2025-06-10 12:06:42 +0900

    Style: restartBtn의 사이즈를 화면의 크기에 따라 유동적으로 변경

--- src/components/PauseSceneScreen.tsx
+++ src/components/PauseSceneScreen.tsx
@@ -6,7 +6,7 @@ const PauseSceneScreen = ({ resumeScene }: { resumeScene: () => void }) => {
             className="fixed top-0 flex h-[100vh] w-[100vw] cursor-pointer items-center justify-center bg-[rgba(0,0,0,0.5)]"
             onClick={resumeScene}
         >
-            <img src={restartBtnSrc} alt="restart-btn" />
+            <img src={restartBtnSrc} alt="restart-btn" className="w-[10vw]" />
         </div>
     );
 };

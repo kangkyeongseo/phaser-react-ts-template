04da30893ef0007bff625046f75c6cec2d55b1b0
kangkyeongseo <kks_big@naver.com>
2026-05-28 09:39:57 +0900

    Feat: Boot Scene에서 Config 로드

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -5,10 +5,18 @@ export class Boot extends Scene {
         super("Boot");
     }
 
-    preload() {}
+    preload() {
+        this.load.setPath("assets");
+        // config
+        this.load.json("config", "game/config.json");
+        this.load.on("loaderror", (file: any) => {
+            if (file.key === "config") {
+                this.cache.json.add("config", {});
+            }
+        });
+    }
 
     create() {
         this.scene.start("Preloader");
     }
 }
-
